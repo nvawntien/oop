41151513193949235f40c7f6239a767fcb9ea457
Nguyen Van Tien <ngvantien1526@gmail.com>
2025-09-24 17:44:56 +0700

    add 5 problem

--- HW1/Introduction.java
+++ HW1/Introduction.java
@@ -3,15 +3,10 @@ public class Introduction {
         System.out.println("Nguyen Van Tien\t24021641\tK69I-CS5\tnvawntien\tngvantien1526@gmail.com");
         
         for (int i = 9; i >= 1; i--) {
-            if (i > 1) {
-                System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
-                System.out.println("Take one down, pass it around,");
-            } 
-            else {
-                System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
-                System.out.println("Take one down, pass it around,");
-                System.out.println("No more bottles of beer on the wall.");
-            }
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
         }
+        
+        System.out.println("No more bottles of beer on the wall.");
     }
 }
